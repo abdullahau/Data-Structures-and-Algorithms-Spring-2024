532c871f9ba621acae506339cc13492eb149ab29
abdullahmahmood <abdullah.au@outlook.com>
2024-05-18 22:29:30 +0400

    update

--- Exercises/Week 4/bigwin.py
+++ Exercises/Week 4/bigwin.py
@@ -1,17 +1,20 @@
 def count(t):
     d = {}
+    counter = {}
     
-    counter = 0
     for i, v in enumerate(t):
         if v not in d:
-            d[v] = []
-        d[v].append(i)
+            d[v] = 1
+        elif v in d:
+            d[v] += 1
+
+        if v//2 in d and v % 2 == 0:
+            if v//2 not in counter:
+                counter[v//2] = 0
+            counter[v//2] += d[v//2]
     
-    for k, v in d.items():
-        if k*2 in d:
-            for i in v:
-                for j in d[k*2]:
-                    if j > i:
-                        counter += 1
-        
-    return counter
+    count = 0
+    for v in counter.values():
+        count += v
+    
+    return count
